2472b1a39527e0b9e5ca474c1db7b55d633000b1
Mordadin <rub.teikari@gmail.com>
2023-04-21 19:06:21 +0000

    yes

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Mordadin/sem03simpleclient
+
+go 1.18
+
+require github.com/Mordadin/is105sem03 v0.0.0-20230421073459-d9671775d649 // indirect
--- tcp_client.go
+++ tcp_client.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
+
+	"github.com/Mordadin/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.2:8500")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +26,10 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4)
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
 }
